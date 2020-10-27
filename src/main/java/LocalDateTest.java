d82ec213411ae046e646375d2a630c8cd2a7ecd1
Fabio Almeida <falmeida.tech@gmail.com>
2020-10-26 21:47:24 -0400

    Testing LocalData Class

---
+++ src/main/java/LocalDateTest.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class LocalDateTest {
+
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+
+    }
+}
