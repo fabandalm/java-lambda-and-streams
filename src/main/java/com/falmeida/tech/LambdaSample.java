3001db256df4275f513098ab79e0409f4d7984cb
Fabio Almeida <falmeida.tech@gmail.com>
2020-10-27 20:52:37 -0400

    Added new lambda declaration

--- src/main/java/com/falmeida/tech/LambdaSample.java
+++ src/main/java/com/falmeida/tech/LambdaSample.java
@@ -1,8 +1,16 @@
 package com.falmeida.tech;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class LambdaSample {
 
     public static void main(String[] args) {
+
+        Consumer<String> consumer = (name) -> System.out.println("Hello " + name);
+        for (String name: Arrays.asList("John","Joane","James")){
+            consumer.accept(name);
+        }
 
         GreetingFunction greeting = (message) -> System.out.println("Java Programming: " + message);
         greeting.sayMessage("Developer");
