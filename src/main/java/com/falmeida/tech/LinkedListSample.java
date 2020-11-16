41b9642d68cb3b2031d42c4ada76d1491f563cf0
Fabio Almeida <falmeida.tech@gmail.com>
2020-11-15 19:40:19 -0500

    Added vector sample

--- src/main/java/com/falmeida/tech/LinkedListSample.java
+++ src/main/java/com/falmeida/tech/LinkedListSample.java
@@ -1,2 +1,34 @@
-package com.falmeida.tech;public class LinkedListSample {
+package com.falmeida.tech;
+
+import java.util.LinkedList;
+
+public class LinkedListSample {
+
+    public static void main(String[] args)
+    {
+
+        // Declaring the LinkedList
+        LinkedList<Integer> ll
+                = new LinkedList<Integer>();
+
+        // Appending new elements at
+        // the end of the list
+        for (int i = 1; i <= 5; i++)
+            ll.add(i);
+
+        // Printing elements
+        System.out.println(ll);
+
+        // Remove element at index 3
+        ll.remove(3);
+
+        // Displaying the List
+        // after deletion
+        System.out.println(ll);
+
+        // Printing elements one by one
+        for (int i = 0; i < ll.size(); i++)
+            System.out.print(ll.get(i) + " ");
+    }
+
 }
