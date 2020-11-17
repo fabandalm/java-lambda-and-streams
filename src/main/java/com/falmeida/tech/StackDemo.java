025ead1f698a533b387a1c54220f91a3d2119747
Fabio Almeida <falmeida.tech@gmail.com>
2020-11-16 23:43:03 -0500

    Added stack demo

---
+++ src/main/java/com/falmeida/tech/StackDemo.java
@@ -0,0 +1,35 @@
+package com.falmeida.tech;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        stack.push("Geeks");
+        stack.push("For");
+        stack.push("Geeks");
+        stack.push("Geeks");
+
+        // Iterator for the stack
+        Iterator<String> itr = stack.iterator();
+
+        // Printing the stack
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+
+        System.out.println();
+
+        stack.pop();
+
+        // Iterator for the stack
+        itr = stack.iterator();
+
+        // Printing the stack
+        while (itr.hasNext()) {
+            System.out.print(itr.next() + " ");
+        }
+    }
+}
