d7dfd401a1704b0024ddf9a752410e7ea6a17543
Fabio Almeida <falmeida.tech@gmail.com>
2020-11-19 23:10:39 -0500

    Added a new currying function

--- src/main/java/com/falmeida/tech/CurryingFunctionDemo.java
+++ src/main/java/com/falmeida/tech/CurryingFunctionDemo.java
@@ -1,2 +1,22 @@
-package com.falmeida.tech;public class CurryingFunctionDemo {
+package com.falmeida.tech;
+
+import java.util.function.Function;
+
+public class CurryingFunctionDemo {
+
+    public static void main(String args[]) {
+
+        // Using Java 8 Functions
+        // to create lambda expressions for functions
+        // and with this, applying Function Currying
+
+        // Curried Function for Adding u & v
+        Function<Integer, Function<Integer, Integer>> curryAdder = u -> v -> u + v;
+
+        // Calling the curried functions
+
+        // Calling Curried Function for Adding u & v
+        System.out.println("Add 2, 3 :" + curryAdder.apply(2).apply(3));
+
+    }
 }
