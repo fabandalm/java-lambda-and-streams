24c760c07fbeb89c5e6d052cee46be77ea98a272
Fabio Almeida <falmeida.tech@gmail.com>
2020-11-22 23:20:10 -0500

    Added hashset collection

---
+++ src/main/java/com/falmeida/tech/HashSetDemo.java
@@ -0,0 +1,25 @@
+package com.falmeida.tech;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetDemo {
+
+    public static void main(String args[]) {
+        // Creating HashSet and
+        // adding elements
+        HashSet<String> hs = new HashSet<String>();
+
+        hs.add("Geeks");
+        hs.add("For");
+        hs.add("Geeks");
+        hs.add("Is");
+        hs.add("Very helpful");
+
+        // Traversing elements
+        Iterator<String> itr = hs.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
